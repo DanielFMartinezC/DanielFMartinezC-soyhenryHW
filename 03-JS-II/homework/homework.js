00992eb905d830d32afe23594fc39a6bf0581c34
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-05-06 10:16:05 -0500

    Update homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -145,16 +145,20 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if(num1 === 0 || num2 === 0 || num3 === 0) {
-    return 'Error'
-  } else if(num1 < 0 || num2 < 0 || num3 < 0) {
-    return 'Hay negativos'
-  } else if((num1 > num2 && num1 > num3) && num1 > 0) {
-    'Número 1 es mayor y positivo'
-  } else if(num3 > num1 && num3 > num2) {
-    return num3 + 1
-  } else {
-    return false
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  else {
+    return false;
   }
 }
 
@@ -198,7 +202,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if(numero > 99) {
+  if(numero > 99 && numero < 1000) {
     return true
   } else {
     return false

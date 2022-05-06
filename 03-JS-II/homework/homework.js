acd01a5fd250cb5518961b6143d6e7ddc2e04002
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-05-06 09:52:03 -0500

    Update homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -152,7 +152,7 @@ function operadoresLogicos(num1, num2, num3) {
     return 'Error'
   } else if(num1 < 0 || num2 < 0 || num3 < 0) {
     return 'Hay negativos'
-  } else if(num1 > num2 && num1 > num3 && num 1 > 0) {
+  } else if(num1 > num2 && num1 > num3 && num1 > 0) {
     'Numero 1 es mayor y positivo'
   } else if(num3 > num1 && num3 > num2) {
     num3++;
